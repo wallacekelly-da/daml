b4b8551280a5540ebb404087d34bce2865f80c68
Kamil Bozek <kamil.bozek@digitalasset.com>
2021-05-25 10:15:11 +0200

    Fixed tests

--- ledger/ledger-api-bench-tool/src/test/suite/scala/com/daml/ledger/api/benchtool/metrics/SizeMetricSpec.scala
+++ ledger/ledger-api-bench-tool/src/test/suite/scala/com/daml/ledger/api/benchtool/metrics/SizeMetricSpec.scala
@@ -17,8 +17,8 @@ class SizeMetricSpec extends AnyWordSpec with Matchers {
       val (_, periodicValue) = metric.periodicValue()
       val finalValue = metric.finalValue(aPositiveDouble())
 
-      periodicValue shouldBe SizeMetric.Value(Some(0.0))
-      finalValue shouldBe SizeMetric.Value(Some(0.0))
+      periodicValue shouldBe SizeMetric.Value(0.0)
+      finalValue shouldBe SizeMetric.Value(0.0)
     }
 
     "compute values after processing elements" in {
@@ -36,8 +36,8 @@ class SizeMetricSpec extends AnyWordSpec with Matchers {
 
       val totalSizeMegabytes =
         (testSizingFunction(elem1) + testSizingFunction(elem2)).toDouble / 1024 / 1024
-      periodicValue shouldBe SizeMetric.Value(Some(totalSizeMegabytes * 1000.0 / periodMillis))
-      finalValue shouldBe SizeMetric.Value(Some(totalSizeMegabytes * 1000.0 / periodMillis))
+      periodicValue shouldBe SizeMetric.Value(totalSizeMegabytes * 1000.0 / periodMillis)
+      finalValue shouldBe SizeMetric.Value(totalSizeMegabytes * 1000.0 / periodMillis)
     }
 
     "correctly handle periods with no elements" in {
@@ -60,8 +60,8 @@ class SizeMetricSpec extends AnyWordSpec with Matchers {
       val firstPeriodMean = firstPeriodMegabytes * 1000.0 / periodMillis
       val secondPeriodMean = 0.0
       val totalMean = (firstPeriodMean + secondPeriodMean) / 2
-      periodicValue shouldBe SizeMetric.Value(Some(secondPeriodMean))
-      finalValue shouldBe SizeMetric.Value(Some(totalMean))
+      periodicValue shouldBe SizeMetric.Value(secondPeriodMean)
+      finalValue shouldBe SizeMetric.Value(totalMean)
     }
 
     "correctly handle multiple periods with elements" in {
@@ -90,8 +90,8 @@ class SizeMetricSpec extends AnyWordSpec with Matchers {
       val thirdPeriodMegabytes = testSizingFunction(elem3).toDouble / 1024 / 1024
       val thirdPeriodMean = thirdPeriodMegabytes * 1000.0 / periodMillis
       val totalMean = (firstPeriodMean + secondPeriodMean + thirdPeriodMean) / 3
-      periodicValue shouldBe SizeMetric.Value(Some(thirdPeriodMean))
-      finalValue shouldBe SizeMetric.Value(Some(totalMean))
+      periodicValue shouldBe SizeMetric.Value(thirdPeriodMean)
+      finalValue shouldBe SizeMetric.Value(totalMean)
     }
   }
 
