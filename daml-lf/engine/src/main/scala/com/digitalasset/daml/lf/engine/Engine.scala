a795630048ea108c72577df93a81560bef2471ee
Nick Chapman <nick.chapman@digitalasset.com>
2021-06-09 10:31:57 +0100

    adapt ledger testcase to change in error message (no partial transaction info)

--- daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/Engine.scala
+++ daml-lf/engine/src/main/scala/com/digitalasset/daml/lf/engine/Engine.scala
@@ -303,7 +303,7 @@ class Engine(val config: EngineConfig = new EngineConfig(LanguageVersion.StableV
           return ResultError(
             Error(
               s"Interpretation error: ${Pretty.prettyError(err).render(80)}",
-              s"Last location: ${Pretty.prettyLoc(machine.lastLocation).render(80)}", //, partial transaction: ${onLedger.ptx.nodesToString}", //NICK
+              s"Last location: ${Pretty.prettyLoc(machine.lastLocation).render(80)}",
             )
           )
 
