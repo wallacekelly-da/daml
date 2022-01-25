2a90cddd1499833b9e9d9ae8714ed911316754e8
Tudor Voicu <tudor.voicu@digitalasset.com>
2022-01-25 16:15:08 +0100

    Higher caches

--- ledger/participant-state/kvutils/app/src/main/scala/com/daml/ledger/participant/state/kvutils/app/ParticipantConfig.scala
+++ ledger/participant-state/kvutils/app/src/main/scala/com/daml/ledger/participant/state/kvutils/app/ParticipantConfig.scala
@@ -40,8 +40,8 @@ object ParticipantConfig {
   // this pool is used for all data access for the ledger api (command submission, transaction service, ...)
   val DefaultApiServerDatabaseConnectionPoolSize = 16
 
-  val DefaultMaxContractStateCacheSize: Long = 100000L
-  val DefaultMaxContractKeyStateCacheSize: Long = 100000L
+  val DefaultMaxContractStateCacheSize: Long = 300000L
+  val DefaultMaxContractKeyStateCacheSize: Long = 300000L
 
   val DefaultMaxTransactionsInMemoryFanOutBufferSize: Long = 10000L
 }
